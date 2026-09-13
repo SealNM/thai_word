fd43d1b24edf4f65646f37428d23ff9167e587d5
Seal <45930810+SealNM@users.noreply.github.com>
2026-09-14 03:53:24 +0700

    chore: default v1 pipeline to bundled dictionary sample

--- thai_lexical_v1.py
+++ thai_lexical_v1.py
@@ -18,6 +18,7 @@ from sklearn.metrics.pairwise import linear_kernel
 
 
 ARTIFACT_VERSION = 1
+DEFAULT_INPUT_PATH = "thai_dictionary_test.json"
 DEFAULT_ID_FIELD = "word_ID"
 DEFAULT_WORD_FIELD = "headword_text"
 DEFAULT_DEFINITION_FIELD = "definition_text"
